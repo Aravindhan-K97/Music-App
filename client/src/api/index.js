79eba371dc78455b85b8598c09c40d446606ede2
Aravindhan-K97 <aravindhan2825@gmail.com>
2025-04-12 22:57:29 +0530

    commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,4 +2,5 @@ import axios from "axios";
 
 export const client = axios.create({
 	baseURL: "https://music-app-9b75.onrender.com/api/",
+	withCredentials: true
 });
--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+	origin: ["http://localhost:5173"], // update this when deployed
+	credentials: true
+  }));
+  
 
 connectDb();
 
